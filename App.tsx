2423d20e02e4c8f7480831f0185995c62d28679b
Ale <duartemattias4@gmail.com>
2023-11-24 18:50:51 -0400

    repaso react

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  }
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Hola Innova Code </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+      <View style={ styles.container }>
+        <StatusBar style="auto"/>
+        <Text style={{ fontSize: 42 }}>
+          { counter }
+        </Text>
+        <Text>Contador</Text>
+        <Button onPress={increment} title="Incrementar" />
+        <Button onPress={decrement} title="Decrementar" />
+      </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, 
+    justifyContent: 'center', 
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  }
+})
